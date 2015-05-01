674c0e474ad45cd2444e07faec4798f404f93df7
Emerson Luiz <eluizbr@gmail.com>
2015-05-01 20:24:13 -0300

    updates

--- install/sql/ramal.sql
+++ install/sql/ramal.sql
@@ -1,10 +1,10 @@
 INSERT INTO `cdr_prefixo` (`ddd`, `prefixo`, `inicial`, `final`, `cidade`, `estado`, `operadora`, `tipo`, `rn1`)
 VALUES
 (0,999999,0,9999,'INTERNO','IN','INTERNO','RAMAL',99999),
-(0,800,0,999,'FIXO','XX','0800','FIXO',8000),
-(0,300,0,999,'FIXO','XX','FIXO','FIXO',300),
-(0,4003,0,999,'FIXO','XX','FIXO','FIXO',4003),
-(0,4004,0,999,'FIXO','XX','FIXO','FIXO',4004),
-(0,500,0,999,'FIXO','XX','FIXO','FIXO',500),
-(0,190,0,999,'POLICIA','XX','LOCAL','FIXO',190),
-(0,900,0,999,'FIXO','XX','FIXO','FIXO',900);
+(0,800,0,999,'FIXO','BR','0800','FIXO',8000),
+(0,300,0,999,'FIXO','BR','FIXO','FIXO',300),
+(0,4003,0,999,'FIXO','BR','FIXO','FIXO',4003),
+(0,4004,0,999,'FIXO','BR','FIXO','FIXO',4004),
+(0,500,0,999,'FIXO','BR','FIXO','FIXO',500),
+(0,190,0,999,'POLICIA','BR','LOCAL','FIXO',190),
+(0,900,0,999,'FIXO','BR','FIXO','FIXO',900);
