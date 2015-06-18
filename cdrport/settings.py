128de9347fec775a56937e65c99d9dd6842a04bb
Emerson Luiz <eluizbr@gmail.com>
2015-06-18 17:13:21 -0300

    Create settings.py

---
+++ cdrport/settings.py
@@ -0,0 +1,119 @@
+"""
+Django settings for portabilidade project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=c_7w=)xh0jx6xyb+li77bc9tnk6l*po)vcjyw*e1_-vxkzpq^'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'core',
+    'django_admin_bootstrapped',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.sites',
+    'registration',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'crispy_forms',
+    'cdr',
+    'smart_selects',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'cdrport.urls'
+
+WSGI_APPLICATION = 'cdrport.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BANCO',
+        'USER': 'USER',
+        'PASSWORD': 'SENHA',
+        'HOST': '192.168.2.85',
+        'PORT': '3306',
+    }
+}
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
+USE_I18N = True
+
+USE_L10N = True
+
+#USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+TEMPLATES_LOADERS = ('django.template.loaders.filesystem.Loader')
+
+# TEMPLATES_LOADERS = ('django.template.loaders.filesystem.Loader',
+#                     'django.template.loaders.app_directories.Loader')
+
+
+STATIC_ROOT = "/cdr/static/"
+                 
+TEMPLATE_DIRS = os.path.join(BASE_DIR, 'templates', 'static'),
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+# CRISPY-FORMS
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+# Django REDUX
+
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = False
+REGISTRATION_OPEN = False
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL='/conta/login/'
+
+# Rate Limit
+
+RATELIMIT_ENABLE = True
